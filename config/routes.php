7b218044f60f29415e1b1494266c33ad0d11bf8c
weliton <welingtonzsilva@gmail.com>
2022-04-07 02:32:35 -0300

    rotas refaturadas

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,24 @@
 <?php
 
-use App\Controller\HomeController;
-use App\Controller\PostController;
-use App\Controller\JsonController;
-use App\Controller\TesteController;
+declare(strict_types=1);
 
-    return  [
-        '/' => HomeController::class.':index',
-        '/json/{name}/{idade}' => JsonController::class.':index',
-        '/post' => PostController::class.':index',
-        '/teste' => TesteController::class.':index'
-    ];
+use App\Controller\HomeController;
+use App\Controller\JsonController;
+use App\Controller\PostController;
+use App\Controller\TesteController;
+use Slim\App;
+use Weliton\InitSlim\Application\Middleware\HelloMiddleware;
+use Weliton\InitSlim\Application\Middleware\NameMiddleware;
+
+use function DI\get;
+
+return function(App $app){
+    $app->get('/',HomeController::class. ':index')
+    ->add(HelloMiddleware::class)
+    ->add(NameMiddleware::class);
+    
+    $app->get('/json/{name}/{idade}',JsonController::class.':index');
+    $app->get('/post', PostController::class.':index');
+    
+    $app->get('/teste', TesteController::class.':index');
+};
